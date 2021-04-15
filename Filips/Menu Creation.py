cf4ce0c82d2e455dbb1980a24388e2513f261bc5
Filips Barakats <philipe.barakat@yahoo.com>
2021-04-15 14:56:34 +0200

    Update Menu Creation.py

--- Filips/Menu Creation.py
+++ Filips/Menu Creation.py
@@ -1,8 +1,8 @@
 import tkinter
 from tkinter import *
 from tkinter import messagebox
-game = Tk()
-game.title("Game Creator")
+app = Tk()
+app.title("Game Creator")
 # game.minsize(640, 480)
 # game.maxsize(1280, 720)
 # game.geometry("1980x1080")
@@ -13,19 +13,33 @@ game.title("Game Creator")
 
 
 # How to center the screen
-screen_x = game.winfo_screenwidth()
-screen_y = game.winfo_screenheight()
+screen_x = app.winfo_screenwidth()
+screen_y = app.winfo_screenheight()
 window_x = 1200
 window_y = 800
 
 pos_X = int((screen_x - window_x) / 2)
 pos_Y = int((screen_y - window_y) / 2)
 
-game.geometry(f"{window_x}x{window_y}+{pos_X}+{pos_Y}")
+app.geometry(f"{window_x}x{window_y}+{pos_X}+{pos_Y}")
 
 # How to create a menu
 
-print("abc")
+main_menu = tkinter.Menu(app)
 
+first_menu = tkinter.Menu(main_menu)
+first_menu.add_command(label="Option 1")
+first_menu.add_command(label="Option 2")
+first_menu.add_command(label="Option 3")
 
-game.mainloop()
+second_menu = tkinter.Menu(main_menu)
+second_menu.add_command(label="Command 1")
+second_menu.add_command(label="Command 2")
+second_menu.add_command(label="Command 3")
+
+main_menu.add_cascade(label="First", menu=first_menu)
+main_menu.add_cascade(label="First", menu=second_menu)
+
+# Main Loop
+app.mainloop()
+app.config(menu=main_menu)
